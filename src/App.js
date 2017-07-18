3d25f13fe8cd2c74f56703c50128efe04c1d0dee
sonlexus <ngocson092@gmail.com>
2017-07-18 16:36:57 +0700

    edit home

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import ChooseModel from './components/ChooseModel';
 import Design from './components/Design';
 import Home from './components/Home';
 import {Route,Link} from 'react-router-dom'
-import { Layout } from 'antd'
+import { Layout, Menu, Icon } from 'antd'
 const { Header, Content } = Layout;
 const Preload = require('react-preload').Preload
 
-
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 class App extends Component {
-
+    state = {
+        current: 'mail',
+    }
+    handleClick = (e) => {
+        console.log('click ', e);
+        this.setState({
+            current: e.key,
+        });
+    }
   render() {
 
     const loadingIndicator = (
@@ -21,7 +30,27 @@ class App extends Component {
     return (
         <Layout>
             <Header>
-                <div className="logo"><Link to={'/'}><span>Your</span> Company</Link></div>
+                <div className="logo"><Link to={'/'}>AZCONA <span>SPORT</span></Link></div>
+
+                <Menu
+                    onClick={this.handleClick}
+                    selectedKeys={[this.state.current]}
+                    mode="horizontal"
+                >
+                    <Menu.Item key="mail">
+                        <Icon type="star-o" />Infield
+                    </Menu.Item>
+                    <Menu.Item key="app">
+                        <Icon type="heart" />Catcher
+                    </Menu.Item>
+                    <SubMenu title={<span><Icon type="windows" />Fastpitch Example</span>}>
+                        <Menu.Item key="setting:1">Option 1</Menu.Item>
+                        <Menu.Item key="setting:2">Option 2</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="as">
+                        <Icon type="api" />Catcher
+                    </Menu.Item>
+                </Menu>
             </Header>
             <Content id="content">
                 <Preload
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React,{Component} from 'react'
 import {connect} from 'react-redux'
-import { Card,Col,Row,Button ,Steps} from 'antd';
+import { Card,Col,Row,Button ,Steps,Carousel } from 'antd';
 
-import {Route} from 'react-router-dom'
+import {Route,Link} from 'react-router-dom'
 import '../stylesheet/_home.scss'
 import classnames from 'classnames'
-import Slider from './include/Slider'
+
 const Step = Steps.Step;
 class HomePage extends Component{
     constructor(props) {
@@ -16,22 +16,22 @@ class HomePage extends Component{
         return (
             <div id="home">
                 <div id="home-intro">
-                    <Slider items={
-                        [
-                            '/images/intro/1.png',
-                            '/images/intro/2.png',
-                            '/images/intro/3.png',
-                        ]
-                    } />
+
+                    <Carousel effect="fade" autoplay>
+                        <div><img src="/images/intro/1.png" alt=""/></div>
+                        <div><img src="/images/intro/2.png" alt=""/></div>
+                        <div><img src="/images/intro/3.png" alt=""/></div>
+                    </Carousel>
+
+                    <Link to="/select_model" className="btn">Customize</Link>
+
                 </div>
 
                 <div id="line-intro">
 
                     <div className="col-md-12 text-center">
-                        <h3 className="font-i">for the love of the game
+                        <h3 className="font-i">Customize Baseball Glove
                         </h3>
-
-
                     </div>
 
 
