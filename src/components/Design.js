fb3e82de075e8e9a1230e83cf600e6f759dbcecc
sonlexus <ngocson092@gmail.com>
2017-07-21 15:54:41 +0700

    edit code

--- src/components/Design.js
+++ src/components/Design.js
@@ -24,8 +24,7 @@ class Design extends Component {
             lacing: 'lace_4',
         },
         part_type_active: 'leather',
-        product: {},
-        angles: {}
+        product: {}
     }
 
     onChange = (e) => {
@@ -90,37 +89,121 @@ class Design extends Component {
 
     render() {
 
-        const radioStyle = {
-            display: 'block',
-            height: '30px',
-            lineHeight: '30px',
-        };
+        const style = {
+            radio: {
+                display: 'block',
+                height: '30px',
+                lineHeight: '30px',
+            },
+            radio_group:{
+                width:"100%"
+            },
+            sidebar: {
+                height: $(window).height() - 64
+            }
+        }
 
-        const loadingIndicator = (
-            <div>
-                <img src="http://www.arunachaluniversity.ac.in/wp-content/uploads/2017/03/ajax-loader.gif" alt=""/>
-            </div>
-        )
+
+        const colors = [
+            '#ffffff', '#929292', '#383838', '#000000',
+            '#f2c250', '#fab400', '#fb5300', '#d61200',
+            '#78000c', '#78000c', '#86105b', '#a35802',
+            '#452006', '#b8d2da', '#006ba6', '#e033a2',
+            '#1a407b', '#0d1f2e', '#2b1b66', '#009d73', '#009639']
 
 
-        const Design = (
+        let panel_data = {
+            leather: [
+                {
+                    name: 'Back Finger',
+                    value: 'fingers'
+                },
+                {
+                    name: 'Web',
+                    value: 'web'
+                },
+                {
+                    name: 'Wrist',
+                    value: 'wrist'
+                },
+                {
+                    name: 'Palm',
+                    value: 'palm'
+                },
+            ],
+            trim: [
+                {
+                    name: 'Binding',
+                    value: 'binding'
+                },
+                {
+                    name: 'Welting Shell',
+                    value: 'beading_1'
+                },
+                {
+                    name: 'Welting Palm',
+                    value: 'beading_2'
+                },
+            ],
+            lacing: [
+                {
+                    name: 'Web',
+                    value: 'lace_4'
+                },
+                {
+                    name: 'Fingertops',
+                    value: 'lace_3'
+                },
+                {
+                    name: 'Heel',
+                    value: 'lace_1'
+                },
+            ]
+        }
+
+
+        const Panels = Object.keys(panel_data).map((part_type)=> {
+
+            let Radio_List = panel_data[part_type].map(item=> {
+                return (
+                    <Radio
+                        style={style.radio}
+                        value={item.value}
+                    >{item.name}
+                    </Radio>
+                )
+            })
+
+            return (
+
+                <Panel header={part_type} key={part_type}>
+                    <RadioGroup
+                        onChange={this.onChange}
+                        value={this.state.part_type[part_type]}
+                        style={style.radio_group}
+                    >
+                        {Radio_List}
+                    </RadioGroup>
+
+                    <div className="color-watch">
+                        <TwitterPicker
+                            colors={colors}
+                            onChangeComplete={ this.handleColorChange}
+                        />
+                    </div>
+                </Panel>
+            )
+        })
+        
+        
+
+        return (
             <div>
                 <Row id="customize">
 
                     <Col xs={12} sm={20} md={18} lg={18} xl={20} className="glove-container">
-
-                        <Preload
-                            loadingIndicator={loadingIndicator}
-                            mountChildren
-                            resolveOnError
-                        >
-                            <div>
-                                <RenderProduct isthumb={true} product={this.state.product} model={this.props.match.params.model}/>
-                                <RenderProduct isthumb={false} product={this.state.product} model={this.props.match.params.model}/>
-                            </div>
-                        </Preload>
-
-
+                        <RenderProduct isthumb={true} product={this.state.product} model={this.props.match.params.model}/>
+                        <RenderProduct isthumb={false} product={this.state.product} model={this.props.match.params.model}/>
                     </Col>
                     <Col xs={12} sm={4} md={6} lg={6} xl={4} className="sidebar"
                          style={{height: $(window).height() - 64}}>
@@ -136,74 +219,7 @@ class Design extends Component {
                         <Collapse onChange={this.handleTypePartChange} accordion bordered={false}
                                   activeKey={this.state.part_type_active} defaultActiveKey={['leather']}
                                   className="customize-glove-parts">
-                            <Panel header={'Leather'} key="leather">
-
-                                <RadioGroup onChange={this.onChange} value={this.state.part_type.leather}
-                                            style={{"width": "100%"}}>
-                                    <Radio style={radioStyle} value={'fingers'}>Back Finger</Radio>
-                                    <Radio style={radioStyle} value={'wrist'}>Wrist</Radio>
-                                    <Radio style={radioStyle} value={'web'}>Web</Radio>
-                                    <Radio style={radioStyle} value={'palm'}>Palm</Radio>
-                                </RadioGroup>
-
-                                <div className="color-watch">
-                                    <TwitterPicker
-                                        colors={[
-                                            '#ffffff', '#929292', '#383838', '#000000',
-                                            '#f2c250', '#fab400', '#fb5300', '#d61200',
-                                            '#78000c', '#78000c', '#86105b', '#a35802',
-                                            '#452006', '#b8d2da', '#006ba6', '#e033a2',
-                                            '#1a407b', '#0d1f2e', '#2b1b66', '#009d73', '#009639']}
-                                        color={ this.state.background }
-                                        onChangeComplete={ this.handleColorChange }
-                                    />
-                                </div>
-
-
-                            </Panel>
-                            <Panel header={'Trim'} key="trim">
-                                <RadioGroup onChange={this.onChange} value={this.state.part_type.trim}
-                                            style={{"width": "100%"}}>
-                                    <Radio style={radioStyle} value={'binding'}>Binding</Radio>
-                                    <Radio style={radioStyle} value={'beading_1'}>Welting Shell</Radio>
-                                    <Radio style={radioStyle} value={'beading_2'}>Welting Palm</Radio>
-                                </RadioGroup>
-
-                                <div className="color-watch">
-                                    <TwitterPicker
-                                        colors={[
-                                            '#ffffff', '#929292', '#383838', '#000000',
-                                            '#f2c250', '#fab400', '#fb5300', '#d61200',
-                                            '#78000c', '#78000c', '#86105b', '#a35802',
-                                            '#452006', '#b8d2da', '#006ba6', '#e033a2',
-                                            '#1a407b', '#0d1f2e', '#2b1b66', '#009d73', '#009639']}
-                                        color={ this.state.background }
-                                        onChangeComplete={ this.handleColorChange }
-                                    />
-                                </div>
-                            </Panel>
-                            <Panel header={'Lacing'} key="lacing">
-                                <RadioGroup onChange={this.onChange} value={this.state.part_type.lacing}
-                                            style={{"width": "100%"}}>
-                                    <Radio style={radioStyle} value={'lace_4'}>Web</Radio>
-                                    <Radio style={radioStyle} value={'lace_3'}>Fingertops</Radio>
-                                    <Radio style={radioStyle} value={'lace_1'}>Heel</Radio>
-                                </RadioGroup>
-
-                                <div className="color-watch">
-                                    <TwitterPicker
-                                        colors={[
-                                            '#ffffff', '#929292', '#383838', '#000000',
-                                            '#f2c250', '#fab400', '#fb5300', '#d61200',
-                                            '#78000c', '#78000c', '#86105b', '#a35802',
-                                            '#452006', '#b8d2da', '#006ba6', '#e033a2',
-                                            '#1a407b', '#0d1f2e', '#2b1b66', '#009d73', '#009639']}
-                                        color={ this.state.background }
-                                        onChangeComplete={ this.handleColorChange }
-                                    />
-                                </div>
-                            </Panel>
-
+                            {Panels}
                         </Collapse>
 
                         <Route render={({history}) => (
@@ -230,7 +246,7 @@ class Design extends Component {
 
         )
 
-        return Design
+
     }
 
 }
