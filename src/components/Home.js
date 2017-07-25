d0f1790061f1c4019a533dea27c2951c259ab612
sonlexus <ngocson092@gmail.com>
2017-07-25 18:03:14 +0700

    add a1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,9 @@
 import React,{Component} from 'react'
 import {connect} from 'react-redux'
-import { Card,Col,Row,Button ,Steps,Carousel } from 'antd';
+import {Carousel } from 'antd';
 
 import {Route,Link} from 'react-router-dom'
 import '../stylesheet/_home.scss'
-import classnames from 'classnames'
-
-const Step = Steps.Step;
 class HomePage extends Component{
     constructor(props) {
         super(props);
@@ -33,8 +30,6 @@ class HomePage extends Component{
                         <h3 className="font-i">Customize Baseball Glove
                         </h3>
                     </div>
-
-
 
                 </div>
 
