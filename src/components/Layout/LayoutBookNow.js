85270ac78f3d38495db2aa1b352015e2349943a4
lamtanphiho <lamtanphiho1@gmail.com>
2017-08-25 02:12:51 +0700

    add login

--- src/components/Layout/LayoutBookNow.js
+++ src/components/Layout/LayoutBookNow.js
@@ -4,14 +4,14 @@ import {Redirect} from 'react-router-dom'
 import {Layout} from 'antd'
 const { Content,Header} = Layout;
 
-// let user = localStorage.getItem("user");
-// let isLogin = true;
-// if (user == 'undefined' || user == null) {
-//     isLogin = false;
-// }
+let user = localStorage.getItem("user");
+let isLogin = true;
+if (user == 'undefined' || user == null) {
+    isLogin = false;
+}
 const LayoutBooknow = ({ children }) => (
     <Layout id="layout-booknow">
-        {/*{!isLogin && ( <Redirect to={'/login'}/>)}*/}
+        {!isLogin && ( <Redirect to={'/login'}/>)}
         <Content id="content">
             {children}
         </Content>
